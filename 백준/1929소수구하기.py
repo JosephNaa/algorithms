d1a93e1bb8f9e377408019479e9224358fc42ea2
JosephNaa <g_ksehd@naver.com>
2021-06-03 02:18:17 +0900

    change account

--- 백준/1929소수구하기.py
+++ 백준/1929소수구하기.py
@@ -1,14 +1,14 @@
 
 def solution(m, n):
 
-    lst = [True for i in range(n+1)]
+    lst = [True for i in range(n + 1)]
 
-    for i in range(2, int((n+1)**0.5)+1):
+    for i in range(2, int((n + 1) ** 0.5) + 1):
         if lst[i]:
-            for j in range(2*i, n+1, i):
+            for j in range(2 * i, n + 1, i):
                 lst[j] = False
 
-    for i in range(m, n+1):
+    for i in range(m, n + 1):
         if i > 1 and lst[i]:
             print(i)
 
