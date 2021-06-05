44354393ae92bb63e8eca5e0e88e9e5802cf0f6d
JosephNaa <js.pekah@gmail.com>
2021-06-05 16:30:43 +0900

    2021-06-05

---
+++ 백준/1568새.py
@@ -0,0 +1,20 @@
+
+def solution(n):
+
+    i = 1
+    count = 0
+
+    while n != 0:
+        if i > n:
+            i = 1
+        n -= i
+        i += 1
+        count += 1
+
+    print(count)
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    solution(n)
