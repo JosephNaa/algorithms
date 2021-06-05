26d199b0bbdbff957444aadae75a9b7d15e84fd5
JosephNaa <js.pekah@gmail.com>
2021-06-05 14:29:46 +0900

    2021-06-05

---
+++ 백준/1543문서검색.py
@@ -0,0 +1,20 @@
+    def solution(data, search):
+
+        count = 0
+        i = 0
+
+        while i < len(data):
+            if data[i:i+len(search)] == search:
+                count += 1
+                i += len(search)
+            else:
+                i += 1
+
+        print(count)
+
+
+    if __name__ == '__main__':
+        data = input()
+        search = input()
+
+        solution(data, search)
