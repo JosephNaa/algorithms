e785363d2101babd817d4462f12195d3ea9ca416
JosephNaa <g_ksehd@naver.com>
2021-06-06 22:24:46 +0900

    수정

--- 백준/1568새.py
+++ 백준/1568새.py
@@ -1,6 +1,5 @@
 
 def solution(n):
-
     i = 1
     count = 0
 
@@ -10,11 +9,9 @@ def solution(n):
         n -= i
         i += 1
         count += 1
-
     print(count)
 
 
 if __name__ == '__main__':
     n = int(input())
-
     solution(n)
