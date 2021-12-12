0c7a0ce6c9532b3a143f805f8900bd864854d3a3
JosephNaa <js.pekah@gmail.com>
2021-12-12 19:23:17 +0900

    Create BOJ11718_그대로출력하기.java

---
+++ 백준/BOJ11718_그대로출력하기.java
@@ -0,0 +1,17 @@
+package js.pekah.algorithms.d1212;
+
+import java.util.Scanner;
+
+public class BOJ11718_그대로출력하기 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextLine()) {
+			String result = sc.nextLine();
+			System.out.println(result);
+		}
+		
+		sc.close();
+	}
+}
