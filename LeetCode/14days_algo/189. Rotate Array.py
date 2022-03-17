ec2cee89133249a1a2e162728cc0bc45f8b7fafb
JosephNaa <js.pekah@gmail.com>
2022-03-18 00:48:22 +0900

    Create 189. Rotate Array.py

---
+++ LeetCode/14days_algo/189. Rotate Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, nums, i, j):
+        li, ri = i, j # 왼쪽, 오른쪽 index
+        
+        while li < ri:
+            tmp = nums[li]
+            nums[li] = nums[ri]
+            nums[ri] = tmp
+            
+            li += 1
+            ri -= 1
+        
+        
+    def rotate(self, nums: List[int], k: int) -> None:
+        k %= len(nums) # k가 101이면 1번만 돌리면 되는데, %로 안나누면 101번 돌아야함
+    
+        self.reverse(nums, 0, len(nums) - k - 1) # 첫번째 파트
+        self.reverse(nums, len(nums) - k, len(nums) - 1) # 두번째 파트
+        self.reverse(nums, 0, len(nums) - 1) # 전체
